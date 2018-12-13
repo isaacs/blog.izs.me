ed115024e31a2be82b263d051f1506f63767a740
isaacs <i@izs.me>
2018-12-12 17:04:59 -0800

    messing around with data and GraphQL

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,6 +7,7 @@ export default ({ data }) => {
   return (
     <Layout>
       <h1>{post.frontmatter.title}</h1>
+      <pre>xxx {JSON.stringify(post.frontmatter, null, 2)}</pre>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
   )
@@ -17,7 +18,56 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
-        title
+        # common fields
+        title # optional!
+        type # text, link, photo, chat, answer, quote, video, audio
+        date
+        slug # just the texty bit of the slug, not the gatsby "slug"
+        tumblrid
+        tags
+        redirects # map from the tumblr urls
+
+        # audio and video
+        embed
+        thumbnail {
+          height
+          url
+          width
+        }
+
+        # video posts
+        html5 # boolean
+        permalink
+        video_type
+        video {
+          youtube { # only youtube has this apparently?
+            video_id
+            height
+            width
+          }
+        }
+        video_url
+
+        # link posts
+        link_url
+        link_publisher
+
+        # audio posts
+        album_art
+        plays
+        track_name
+
+        # photo posts
+        photoset_layout
+        photos {
+          url {
+            absolutePath # these get turned into local file objects?
+            relativePath
+          }
+          height
+          width
+          alt
+        }
       }
     }
   }
