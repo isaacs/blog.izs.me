39146b1c2d62935b69c808cc8f1aa34589208d94
isaacs <i@izs.me>
2018-12-20 14:29:28 -0500

    a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout headerText="not found">
+    <div class="post" style={{
+      textAlign: 'center',
+      fontSize:'2em',
+      padding:'2em'
+    }}>
+      i looked but its not there
+    </div>
+  </Layout>
+)
