48e3ef245f9c2a60145ac687b4192b93e9cbd39f
isaacs <i@izs.me>
2023-02-23 10:21:16 -0800

    sort tags

--- .eleventy.js
+++ .eleventy.js
@@ -6,13 +6,15 @@ const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPassthroughCopy('src/**/*.{jpg,png,gif,mp3,mp4,pdf,css,ico}')
+  eleventyConfig.addPassthroughCopy(
+    "src/**/*.{jpg,png,gif,mp3,mp4,pdf,css,ico}"
+  );
 
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
@@ -20,140 +22,138 @@ module.exports = function(eleventyConfig) {
   // Alias `layout: post` to `layout: layouts/post.njk`
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addFilter('escapeHashes', s => {
-    return s.split('#').join(escape('#'))
-  })
+  eleventyConfig.addFilter("escapeHashes", (s) => {
+    return s.split("#").join(escape("#"));
+  });
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy-LL-dd");
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
-    if(!Array.isArray(array) || array.length === 0) {
+    if (!Array.isArray(array) || array.length === 0) {
       return [];
     }
-    if( n < 0 ) {
+    if (n < 0) {
       return array.slice(n);
     }
 
     return array.slice(0, n);
   });
 
-  eleventyConfig.addFilter('thisYear', (posts, min) => {
-    const year = new Date(new Date() - 1000*60*60*24*365)
-    const thisYearPosts = posts.filter(p => p.date > year)
-    return thisYearPosts.length > min ? thisYearPosts : posts.slice(-1 * min)
-  })
+  eleventyConfig.addFilter("thisYear", (posts, min) => {
+    const year = new Date(new Date() - 1000 * 60 * 60 * 24 * 365);
+    const thisYearPosts = posts.filter((p) => p.date > year);
+    return thisYearPosts.length > min ? thisYearPosts : posts.slice(-1 * min);
+  });
 
   // Return the smallest number argument
   eleventyConfig.addFilter("min", (...numbers) => {
     return Math.min.apply(null, numbers);
-  })
+  });
 
   function filterTagList(tags) {
-    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+    return (tags || []).filter(
+      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+    );
   }
 
-  const cheerio = require('cheerio')
-  const { dirname, resolve } = require('path')
+  const cheerio = require("cheerio");
+  const { dirname, resolve } = require("path");
   eleventyConfig.addNunjucksFilter("localImage", (content, page) => {
-    const $ = cheerio.load(content)
-    for (const img of $('img[src]')) {
-      const { src } = img.attribs
-      if (/^https?:/.test(src))
-        continue
-      img.attribs.src = resolve(dirname(page.filePathStem), img.attribs.src)
+    const $ = cheerio.load(content);
+    for (const img of $("img[src]")) {
+      const { src } = img.attribs;
+      if (/^https?:/.test(src)) continue;
+      img.attribs.src = resolve(dirname(page.filePathStem), img.attribs.src);
     }
-    return $('body').html()
-  })
+    return $("body").html();
+  });
 
-  eleventyConfig.addNunjucksAsyncFilter("tumble", require('@isaacs/njk-tumble'))
+  eleventyConfig.addNunjucksAsyncFilter(
+    "tumble",
+    require("@isaacs/njk-tumble")
+  );
   eleventyConfig.addNunjucksFilter("draftPermalink", (slug, page) => {
-    const { date, fileSlug } = page
-    if (!slug)
-      slug = fileSlug
-    return `draft/${slug}`
-  })
+    const { date, fileSlug } = page;
+    if (!slug) slug = fileSlug;
+    return `draft/${slug}`;
+  });
 
   eleventyConfig.addNunjucksFilter("postPermalink", (slug, page) => {
-    const { date, fileSlug } = page
-    if (!slug)
-      slug = fileSlug
-    const p = Date.parse(date)
-    if (!p)
-      return `/${slug}`
-    const d = new Date(p)
-    const pad = (n, s) => ('0'.repeat(s) + n.toString(10)).slice(-1 * s)
-    const y = pad(d.getUTCFullYear(), 4)
-    const m = pad(d.getUTCMonth() + 1, 2)
-    return `/${y}/${m}/${slug}`
-  })
+    const { date, fileSlug } = page;
+    if (!slug) slug = fileSlug;
+    const p = Date.parse(date);
+    if (!p) return `/${slug}`;
+    const d = new Date(p);
+    const pad = (n, s) => ("0".repeat(s) + n.toString(10)).slice(-1 * s);
+    const y = pad(d.getUTCFullYear(), 4);
+    const m = pad(d.getUTCMonth() + 1, 2);
+    return `/${y}/${m}/${slug}`;
+  });
 
-  const { kebabCase } = require('lodash')
-  eleventyConfig.addNunjucksFilter("tagPermalink", (tag) =>
-    `/tagged/${kebabCase(tag)}`)
+  const { kebabCase } = require("lodash");
+  eleventyConfig.addNunjucksFilter(
+    "tagPermalink",
+    (tag) => `/tagged/${kebabCase(tag)}`
+  );
 
-  eleventyConfig.addFilter("filterTagList", filterTagList)
+  eleventyConfig.addFilter("filterTagList", filterTagList);
 
   // Create an array of all tags
-  eleventyConfig.addCollection("tagList", function(collection) {
+  eleventyConfig.addCollection("tagList", function (collection) {
     let tagSet = new Set();
-    collection.getAll().forEach(item => {
-      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    collection.getAll().forEach((item) => {
+      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
     });
 
-    return filterTagList([...tagSet]);
+    return filterTagList([...tagSet].sort((a, b) => a.localeCompare(b, "en")));
   });
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
     html: true,
     breaks: false,
-    linkify: true
+    linkify: true,
   }).use(markdownItAnchor, {
     permalink: markdownItAnchor.permalink.ariaHidden({
       placement: "after",
       class: "direct-link",
       symbol: "#",
-      level: [1,2,3,4],
+      level: [1, 2, 3, 4],
     }),
-    slugify: eleventyConfig.getFilter("slug")
-  })
+    slugify: eleventyConfig.getFilter("slug"),
+  });
   eleventyConfig.setLibrary("md", markdownLibrary);
 
   // Override Browsersync defaults (used only with --serve)
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("_site/404.html");
 
         browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
-          res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
           res.write(content_404);
           res.end();
         });
       },
     },
     ui: false,
-    ghostMode: false
+    ghostMode: false,
   });
 
   return {
     // Control which files Eleventy will process
     // e.g.: *.md, *.njk, *.html, *.liquid
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+    templateFormats: ["md", "njk", "html", "liquid"],
 
     // -----------------------------------------------------------------
     // If your site deploys to a subdirectory, change `pathPrefix`.
@@ -183,7 +183,7 @@ module.exports = function(eleventyConfig) {
       input: "src",
       includes: "_includes",
       data: "_data",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
--- src/posts/2009/03/a-new-structure/index.md
+++ src/posts/2009/03/a-new-structure/index.md
@@ -5,13 +5,11 @@ slug: a-new-structure
 date: 2009-03-28T20:00:00.000Z
 tags:
   - snipsblog
-  - >-
-    move new structure to its proper place.  fix new.sh so that it doesn't keep
-    breaking.
 redirect_from:
   - /snips/2009-03-28/a-new-structure/
   - /snips/2009-03-28/a-new-structure
 ---
+
 I updated the structure of my snips.
 
 As part of the commit of this change, I'm going to also have this snip.
