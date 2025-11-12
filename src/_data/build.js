d25ead8b951f6fc01b098734fecc58e4f875a531
isaacs <i@izs.me>
2025-11-11 23:02:59 -0800

    pull release string from package.json

--- src/_data/build.js
+++ src/_data/build.js
@@ -1,3 +1,6 @@
+const pkg = require('../../package.json')
 module.exports = {
-  env: process.env.npm_command === 'start' ? 'dev' : 'production'
+  env: process.env.npm_command === 'start' ? 'dev' : 'production',
+  package: pkg,
+  release: `${pkg.name}@${pkg.version}`,
 }
