f17322602b86853b8427b27b8e07fb1bd39b67a5
isaacs <i@izs.me>
2025-11-11 16:38:17 -0800

    noisier ignore script

--- ignore-main.sh
+++ ignore-main.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [ "$(git branch | grep \*)" = "* main" ]; then
+b="$(git branch | grep \*)"
+if [ "$b" = "* main" ]; then
   echo "on main branch, skip automatic build"
   exit 0
+else
+  echo "branch=$b, do not ignore"
+  exit 1
 fi
-exit 1
